8e4131bbd502e8c15b1cf492d94ee9174ad5caaf
Santiago Robledo <santiddf9@gmail.com>
2025-06-26 17:04:45 -0300

    arregle cosas del punto 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,16 +1,14 @@
-// Función para generar la tabla de multiplicar
+// 7- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla,
+// la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del
+// 1 al 10 del número elegido por el usuario.
 function generarTablaMultiplicar(numero) {
-  document.writeln(`Tabla de multiplicar del ${numero}:`);
-  
+  document.writeln(`Tabla de multiplicar del ${numero}:`);  
   for (let i = 1; i <= 10; i++) {
     document.writeln(`${numero}x${i}=${numero * i} - `);
   }
 }
 
-// Solicitar número al usuario
 const numero = parseInt(prompt("Ingrese un número para ver su tabla de multiplicar:"));
-
-// Verificar que sea un número válido
 if (!isNaN(numero)) {
   generarTablaMultiplicar(numero);
 } else {
